e52921b377bb3434be73f6af03fcff93a5e1fa7f
Isa Muriel <isamuriel@MacBook-Pro-de-Isa.local>
2020-02-25 21:34:16 -0500

    Adding laravel course

---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Name</div>
+
+                <div class="card-body">
+                    Isabela Muriel Roldán
+                </div>
+            </div> <br>
+            <div class="card">
+                <div class="card-header">Address</div>
+
+                <div class="card-body">
+                    cll 26 sur # 43A 41
+                </div>
+            </div> <br>
+            <div class="card">
+                <div class="card-header">Phone</div>
+
+                <div class="card-body">
+                    3206141485
+                </div>
+            </div> <br>
+            <div class="card">
+                <div class="card-header">Email</div>
+
+                <div class="card-body">
+                    imurielr@eafit.edu.co
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/product/saved.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Submitted</div>
+
+                <div class="card-body">
+                    Product Created
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
